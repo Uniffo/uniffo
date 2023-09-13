e30f89fe8a9465a9004b80dac6115dcb22c5ea81
Maciej Koralewski <m.koralewski.98@gmail.com>
2023-09-13 23:52:04 +0200

    feature: add util: formatDate

---
+++ src/utils/Date/formatDate.d.ts
@@ -0,0 +1,1 @@
+export type TFormatDate = (date: Date) => string;
---
+++ src/utils/Date/formatDate.ts
@@ -0,0 +1,13 @@
+import { TFormatDate } from './formatDate.d.ts';
+
+export const formattedDate: TFormatDate = (date) => {
+	const day = date.getDate().toString().padStart(2, '0');
+	const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Note: Months are zero-based, so we add 1.
+	const year = date.getFullYear().toString();
+	const hours = date.getHours().toString().padStart(2, '0');
+	const minutes = date.getMinutes().toString().padStart(2, '0');
+	const seconds = date.getSeconds().toString().padStart(2, '0');
+	const milliseconds = date.getMilliseconds().toString().padStart(3, '0');
+
+	return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.${milliseconds}`;
+};
