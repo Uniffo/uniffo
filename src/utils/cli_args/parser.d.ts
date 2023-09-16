f67db65ff8b399b71138be1f4303c037a5aa43d0
Maciej Koralewski <m.koralewski.98@gmail.com>
2023-09-16 19:17:02 +0200

    feature: update parse

--- src/utils/cli_args/parser.d.ts
+++ src/utils/cli_args/parser.d.ts
@@ -2,5 +2,6 @@ import { IUniffoArgs } from '../../commands/uniffo/uniffo.d.ts';
 
 interface ICliArgs extends IUniffoArgs {
 	[key: string]: string | undefined;
+	debug?: string | undefined;
 }
 export type TParseCliArgs = (denoArgs: string[]) => ICliArgs;
