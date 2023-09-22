1b1dc966778e540dd31650d3fc19e25d10e5c0c2
Maciej Koralewski <m.koralewski.98@gmail.com>
2023-09-22 23:01:43 +0200

    feature: updatetest for pwd util function

--- src/utils/workdir/pwd.test.ts
+++ src/utils/workdir/pwd.test.ts
@@ -1,8 +1,9 @@
 import { assertEquals } from 'https://deno.land/std@0.201.0/assert/assert_equals.ts';
-import { LOGGER, UNIFFO_PROJECT_TOP_LEVEL_STRUCTURE } from '../../constants/constants.ts';
+import { UNIFFO_PROJECT_TOP_LEVEL_STRUCTURE } from '../../constants/constants.ts';
 import { pathExist } from '../path/exist.ts';
 import { cwd } from './cwd.ts';
 import { pwd } from './pwd.ts';
+import { logger } from '../../services/logger.ts';
 
 Deno.test('pwd', async function testPwd() {
 	const getRandomDig = () => Math.floor(Math.random() * (9 - 1)) + 1;
@@ -21,11 +22,11 @@ Deno.test('pwd', async function testPwd() {
 	const testingDirPath = await getTestDirPath();
 	const parentDirPath = cwd();
 
-	LOGGER.debug(`Var testingDirPath: '${testingDirPath}'`);
+	logger.debug(`Var testingDirPath: '${testingDirPath}'`);
 
-	LOGGER.debug(`Creating environment for test`);
+	logger.debug(`Creating environment for test`);
 
-	LOGGER.debug(`Creating '${testingDirPath}'`);
+	logger.debug(`Creating '${testingDirPath}'`);
 	Deno.mkdirSync(testingDirPath);
 
 	for (let i = 0; i < UNIFFO_PROJECT_TOP_LEVEL_STRUCTURE.length; i++) {
@@ -33,7 +34,7 @@ Deno.test('pwd', async function testPwd() {
 		const isFile = partOfPath.includes('.');
 		const pathToCreate = `${testingDirPath}/${partOfPath}`;
 
-		LOGGER.debug(`Creating '${pathToCreate}'`);
+		logger.debug(`Creating '${pathToCreate}'`);
 
 		if (isFile) {
 			Deno.writeFileSync(pathToCreate, new TextEncoder().encode(''));
@@ -42,23 +43,23 @@ Deno.test('pwd', async function testPwd() {
 		}
 	}
 
-	LOGGER.debug(`Change directory to '${testingDirPath}'`);
+	logger.debug(`Change directory to '${testingDirPath}'`);
 
 	Deno.chdir(testingDirPath);
 
-	LOGGER.debug(`Testing pwd in project'`);
+	logger.debug(`Testing pwd in project'`);
 
 	assertEquals(await pwd(), testingDirPath, 'Pwd in project');
 
-	LOGGER.debug(`Change directory to '${parentDirPath}'`);
+	logger.debug(`Change directory to '${parentDirPath}'`);
 
 	Deno.chdir(parentDirPath);
 
-	LOGGER.debug(`Testing pwd out of project'`);
+	logger.debug(`Testing pwd out of project'`);
 
 	assertEquals(await pwd(), false, 'Pwd out of project');
 
-	LOGGER.debug(`Removing '${testingDirPath}'`);
+	logger.debug(`Removing '${testingDirPath}'`);
 
 	Deno.removeSync(testingDirPath, { recursive: true });
 });
