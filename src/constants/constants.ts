7ef42e8c6a8450b9ef798b80f11a2f1efe99d2d2
Maciej Koralewski <m.koralewski.98@gmail.com>
2023-09-29 18:13:01 +0200

    feature: update const UNIFFO_DIR

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,4 +2,8 @@ import { parseCliArgs } from '../utils/cli_args/parser.ts';
 
 export const CLI_ARGS = parseCliArgs(Deno.args);
 export const UNIFFO_PROJECT_TOP_LEVEL_STRUCTURE = ['uniffo', 'data', 'source', '.uvm'];
-export const UNIFFO_DIR = `${Deno.env.get('HOME')}/.uniffo`;
+export const UNIFFO_DIR = {
+	main: `${Deno.env.get('HOME')}/.uniffo`,
+	tmp: `${Deno.env.get('HOME')}/tmp`,
+	versions: `${Deno.env.get('HOME')}/.uniffo/versions`,
+};
