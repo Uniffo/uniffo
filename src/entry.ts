7d63b9d1b12620d805d62a4328bf8c0ee51acff1
Maciej Koralewski <m.koralewski.98@gmail.com>
2023-09-29 19:31:48 +0200

    feature: update entry

--- src/entry.ts
+++ src/entry.ts
@@ -1,23 +1,29 @@
 import { uniffo } from './commands/uniffo/uniffo.ts';
 import { CLI_ARGS } from './constants/constants.ts';
 import { logger } from './services/logger.ts';
-import { store } from './services/store.ts';
+import { session } from './services/session.ts';
 import { uvm } from './services/uvm.ts';
 import { pwd } from './utils/workdir/pwd.ts';
 
 await (async function cliEntry() {
 	logger.debug(`Var cliArgs: ${JSON.stringify(CLI_ARGS)}`);
 
-	await uvm.init();
+	try {
+		await session.init();
 
-	const projectWorkingDir = await pwd();
-	const isProjectInitialized = !!projectWorkingDir;
+		await uvm.init();
 
-	if (isProjectInitialized) {
-		// TODO(#2): uvm
+		const projectWorkingDir = await pwd();
+		const isProjectInitialized = !!projectWorkingDir;
+
+		if (isProjectInitialized) {
+			// TODO(#2): uvm
+		}
+
+		uniffo(CLI_ARGS);
+	} catch (error) {
+		logger.error(`${error}`);
 	}
 
-	uniffo(CLI_ARGS);
-
-	store.deleteSession();
+	await session.destroy();
 })();
