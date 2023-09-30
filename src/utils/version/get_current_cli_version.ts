4a36c0402fec707949b25d3d2b1a9efca617033a
Maciej Koralewski <m.koralewski.98@gmail.com>
2023-09-30 21:09:28 +0200

    feature: add documentation to utility function "getCurrentCliVersion"

--- src/utils/version/get_current_cli_version.ts
+++ src/utils/version/get_current_cli_version.ts
@@ -2,6 +2,12 @@ import { PRECOMPILED_DIR } from '../../constants/constants.ts';
 import { logger } from '../../services/logger.ts';
 import { pathExist } from '../path/exist.ts';
 
+/**
+ * The function `getCurrentCliVersion` checks if a specific module exists and if so, imports it and
+ * returns the default value as a string, otherwise it returns false.
+ * @returns The function `getCurrentCliVersion` returns either the version string if it is found, or
+ * `false` if the version is not found.
+ */
 export const getCurrentCliVersion = async () => {
 	const cliVersionModuleFilename = `${PRECOMPILED_DIR}/__cli_version.ts`;
 	logger.debug(`Var cliVersionModuleFilename: "${cliVersionModuleFilename}"`);
