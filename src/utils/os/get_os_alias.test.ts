5256bb44d192fb412f4f193a998206c1a3cfa014
Maciej Koralewski <m.koralewski.98@gmail.com>
2023-11-29 20:11:33 +0100

    test: update test coverage for fn getOsAlias

--- src/utils/os/get_os_alias.test.ts
+++ src/utils/os/get_os_alias.test.ts
@@ -6,4 +6,5 @@ Deno.test('getOsAlias', () => {
 	assertEquals(getOsAlias('linux', 'x86_64'), 'linux_x64', 'test linux_x64');
 	assertEquals(getOsAlias('darwin', 'x86_64'), 'macos_x64', 'test macos_x64');
 	assertEquals(getOsAlias('darwin', 'aarch64'), 'macos_arm', 'test macos_arm');
+	assertEquals(getOsAlias().length > 1, true, 'test no value');
 });
