f210ad7316ab052ada078ed9dc73df18d7a359a4
Maciej Koralewski <m.koralewski.98@gmail.com>
2023-12-05 18:39:11 +0100

    test: update test coverage for fn getCallingFunctionName

--- src/utils/calling_function_name/calling_function_name.ts
+++ src/utils/calling_function_name/calling_function_name.ts
@@ -1,22 +1,34 @@
 /**
  * The function `getCallingFunctionName` retrieves the name of the calling function in TypeScript.
+ * @param [offset=4] - The `offset` parameter is used to determine the position in the call stack to
+ * retrieve the calling function name. By default, it is set to 4, which means it will retrieve the
+ * calling function name that is 4 levels above the `getCallingFunctionName` function in the call
+ * stack.
+ * @param [unknown=false] - The `unknown` parameter is a boolean flag that determines whether to return
+ * "Unknown" if the calling function name cannot be determined. If `unknown` is set to `true`, the
+ * function will always return "Unknown" regardless of whether the calling function name can be
+ * determined or not.
  * @returns The function `getCallingFunctionName` returns the name of the calling function. If the
- * calling function name cannot be determined, it returns the string "Unknown".
+ * stack trace is not available or the `unknown` parameter is set to `true`, it returns the string
+ * "Unknown".
  */
-export const getCallingFunctionName = () => {
+export const getCallingFunctionName = (offset = 4, unknown = false) => {
 	const stack = new Error().stack;
 	const Unknown = 'Unknown';
 
-	if (!stack) {
+	if (!stack || unknown) {
 		return Unknown;
 	}
 
 	const stackLines = stack.split('\n');
-	const callerLine = stackLines[4].trim();
+	console.log('LOG: stackLines', stackLines);
+	const callerLine = stackLines[offset].trim();
+	console.log('LOG: callerLine', callerLine);
 	const callerFunctionName = callerLine.substring(
 		callerLine.indexOf(' ') + 1,
 		callerLine.lastIndexOf(' ('),
 	);
+	console.log('LOG: callerFunctionName', callerFunctionName);
 
 	if (!callerFunctionName.length || callerFunctionName === 'at ') {
 		return Unknown;
