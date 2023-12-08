a8a6cf5c1c535e2940204702bd21135a2b39399b
Maciej Koralewski <m.koralewski.98@gmail.com>
2023-12-08 16:57:14 +0100

    docs: update enging class docs

--- src/classes/engine/engine.ts
+++ src/classes/engine/engine.ts
@@ -13,6 +13,15 @@ export class classEngine {
 		this.session = args.session;
 	}
 
+	/**
+	 * The function `exec` is an asynchronous function that takes in arguments and a preferred Uniffo
+	 * version, initializes a session, initializes the Uniffo version manager, and then either dispatches
+	 * a Uniffo command or executes a command based on certain conditions.
+	 * @param args - An array of command line arguments passed to the `exec` function.
+	 * @param {version} [prefferedUniffoVersion] - The `prefferedUniffoVersion` parameter is an optional
+	 * argument that specifies the preferred version of Uniffo to use. If provided, the `init` method of
+	 * the `uniffoVersionManager` is called with this version to initialize the Uniffo version manager.
+	 */
 	public async exec(args: typeof Deno.args, prefferedUniffoVersion?: version) {
 		logger.debug(`Args: "${JSON.stringify(args)}"`);
 
@@ -39,6 +48,12 @@ export class classEngine {
 		}
 	}
 
+	/**
+	 * The function `ensureUniffoPermissions` checks if a file at a given path has execute permissions and
+	 * upgrades the permissions if necessary.
+	 * @param {string} path - The `path` parameter is a string that represents the file path of the
+	 * dispatch target.
+	 */
 	private async ensureUniffoPermissions(path: string) {
 		const dispatchTargetMode = (await Deno.stat(path)).mode;
 		const executePermission = 0o111;
@@ -52,6 +67,14 @@ export class classEngine {
 		}
 	}
 
+	/**
+	 * The function dispatches a command with the given path and arguments and waits for the process to
+	 * complete.
+	 * @param {string} path - A string representing the path to the command or executable file that you
+	 * want to run.
+	 * @param args - The `args` parameter is an array of strings representing the command line arguments
+	 * to be passed to the command being executed.
+	 */
 	private async dispatchCommand(path: string, args: typeof Deno.args) {
 		logger.debug(`Dispatch "${path} ${args.join(' ')}"`);
 
@@ -64,6 +87,11 @@ export class classEngine {
 		await process.status;
 	}
 
+	/**
+	 * The function `getDispatchTarget` retrieves the dispatch target from the `uniffoVersionManager` and
+	 * checks if it exists, throwing an error if it doesn't.
+	 * @returns the value of the variable `dispatchTarget`.
+	 */
 	private async getDispatchTarget() {
 		const dispatchTarget = this.uniffoVersionManager.getDispatchTarget();
 		logger.debug(`Var dispatchTarget: ${dispatchTarget}`);
