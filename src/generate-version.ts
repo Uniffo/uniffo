b2fca4a31fa1c09b9c8a05c48122bc1a569c95b6
Maciej Koralewski <m.koralewski.98@gmail.com>
2023-12-09 16:41:38 +0100

    refactor: generate version script

--- src/generate-version.ts
+++ src/generate-version.ts
@@ -21,7 +21,7 @@ import { cwd } from './utils/workdir/cwd.ts';
 		.outputSync();
 	const latestReleaseTag = textDecoder.decode(
 		latestReleaseTagCmd.stdout,
-	);
+	).trim();
 
 	error = textDecoder.decode(
 		latestReleaseTagCmd.stderr,
@@ -29,7 +29,7 @@ import { cwd } from './utils/workdir/cwd.ts';
 
 	if (error) throw error;
 
-	logger.debug(`latestReleaseTag: "${latestReleaseTag.trim()}"`);
+	logger.debug(`latestReleaseTag: "${latestReleaseTag}"`);
 
 	// Get commits range
 	const commitsRangeCmd = new Deno.Command(getNewReleaseCommitsRange, {
@@ -37,7 +37,7 @@ import { cwd } from './utils/workdir/cwd.ts';
 	}).outputSync();
 	const commitsRange = textDecoder.decode(
 		commitsRangeCmd.stdout,
-	);
+	).trim();
 
 	error = textDecoder.decode(
 		commitsRangeCmd.stderr,
@@ -48,29 +48,33 @@ import { cwd } from './utils/workdir/cwd.ts';
 	const commitStart = commitsRange.split(',')[0];
 	const commitStop = commitsRange.split(',')[1];
 
-	logger.debug(`commitsRange: "${commitsRange.trim()}"`);
+	logger.debug(`commitsRange: "${commitsRange}"`);
 
 	// Get next semantic version
-	const nextSemanticVersionCmd = new Deno.Command(getNextSemanticVersion, {
-		args: [latestReleaseTag, commitStart, commitStop],
-	})
-		.outputSync();
-	const nextSemanticVersion = textDecoder.decode(
-		nextSemanticVersionCmd.stdout,
-	);
+	let nextSemanticVersion = latestReleaseTag;
 
-	error = textDecoder.decode(
-		nextSemanticVersionCmd.stderr,
-	);
+	if (commitStart !== commitStop) {
+		const nextSemanticVersionCmd = new Deno.Command(getNextSemanticVersion, {
+			args: [latestReleaseTag, commitStart, commitStop],
+		})
+			.outputSync();
+		nextSemanticVersion = textDecoder.decode(
+			nextSemanticVersionCmd.stdout,
+		).trim();
 
-	if (error) throw error;
+		error = textDecoder.decode(
+			nextSemanticVersionCmd.stderr,
+		);
 
-	logger.debug(`nextSemanticVersion: "${nextSemanticVersion.trim()}"`);
+		if (error) throw error;
+	}
+
+	logger.debug(`nextSemanticVersion: "${nextSemanticVersion}"`);
 
 	// Write to file
 	const versionFile = `${workdir}/VERSION`;
 
-	logger.debug(`write to file "${nextSemanticVersion.trim()}"`);
+	logger.debug(`write to file "${nextSemanticVersion}"`);
 
-	Deno.writeTextFileSync(versionFile, nextSemanticVersion.trim());
+	Deno.writeTextFileSync(versionFile, nextSemanticVersion);
 })();
