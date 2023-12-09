b353ef54f4842c57410f241e1501b281117700e1
Maciej Koralewski <m.koralewski.98@gmail.com>
2023-12-09 23:02:26 +0100

    refactor: change way of getting current cli version as a ts module

--- src/utils/version/get_current_cli_version.test.ts
+++ src/utils/version/get_current_cli_version.test.ts
@@ -1,25 +1,6 @@
 import { assertEquals } from 'https://deno.land/std@0.201.0/assert/assert_equals.ts';
-import { PRECOMPILED_DIR } from '../../constants/constants.ts';
-import { pathExist } from '../path/exist.ts';
 import { getCurrentCliVersion } from './get_current_cli_version.ts';
-import { getError } from '../error/get_error.ts';
 
 Deno.test('getCurrentCliVersion', async function testGetCurrentCliVersion() {
 	assertEquals(typeof await getCurrentCliVersion() === 'string', true, 'version');
-
-	if (await pathExist(PRECOMPILED_DIR)) {
-		await Deno.rename(PRECOMPILED_DIR, `${PRECOMPILED_DIR}_old`);
-	}
-
-	assertEquals(
-		(await getError<string>(async () => {
-			await getCurrentCliVersion();
-		})).length > 0,
-		true,
-		'no version',
-	);
-
-	if (await pathExist(`${PRECOMPILED_DIR}_old`)) {
-		await Deno.rename(`${PRECOMPILED_DIR}_old`, PRECOMPILED_DIR);
-	}
 });
