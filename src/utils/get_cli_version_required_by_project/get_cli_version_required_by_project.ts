20508dd64c2be2201754bd2dfabca82d4fb1f454
Maciej Koralewski <m.koralewski.98@gmail.com>
2024-03-24 16:55:35 +0100

    refactor(#14): update logs for getCliVersionRequiredByProject function

--- src/utils/get_cli_version_required_by_project/get_cli_version_required_by_project.ts
+++ src/utils/get_cli_version_required_by_project/get_cli_version_required_by_project.ts
@@ -1,6 +1,7 @@
 import { version } from '../../classes/cli_version_manager/cli_version_manager.d.ts';
 import { CLI_PVFB } from '../../constants/CLI_PVFB.ts';
 import { pwd } from '../pwd/pwd.ts';
+import { logger } from '../../global/logger.ts';
 
 /**
  * The function `getCliVersionRequiredByProject` reads the contents of a file named `CLI_PVFB` in
@@ -8,15 +9,21 @@ import { pwd } from '../pwd/pwd.ts';
  * @returns The function `getCliVersionRequiredByProject` returns the version number read from the file
  * specified by `projectWpdVersionFilename`.
  */
-export const getCliVersionRequiredByProject = async () => {
+export async function getCliVersionRequiredByProject() {
+	logger.debugFn(arguments);
+
 	const projectWorkDir = await pwd();
+	logger.debugVar('projectWorkDir', projectWorkDir);
 
 	if (!projectWorkDir) {
 		return false;
 	}
 
 	const projectWpdVersionFilename = `${projectWorkDir}/${CLI_PVFB}`;
-	const version = await Deno.readTextFile(projectWpdVersionFilename);
+	logger.debugVar('projectWpdVersionFilename', projectWpdVersionFilename);
 
-	return version.trim() as version;
-};
+	const version = (await Deno.readTextFile(projectWpdVersionFilename)).trim() as version;
+	logger.debugVar('version', version);
+
+	return version;
+}
