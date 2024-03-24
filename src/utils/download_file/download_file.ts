9cc317003ab92494ee473316f97fcce04b34a6db
Maciej Koralewski <m.koralewski.98@gmail.com>
2024-03-24 15:55:32 +0100

    refactor(#14): update logs for downloadFile function

--- src/utils/download_file/download_file.ts
+++ src/utils/download_file/download_file.ts
@@ -1,4 +1,5 @@
 import { pathExist } from '../path_exist/path_exist.ts';
+import { logger } from '../../global/logger.ts';
 
 /**
  * The `downloadFile` function is a TypeScript function that downloads a file from a given URL, saves
@@ -6,11 +7,14 @@ import { pathExist } from '../path_exist/path_exist.ts';
  * @param arg - The `arg` parameter is an object that contains the following properties:
  * @returns The function `downloadFile` returns an object with the following properties:
  */
-export const downloadFile = async (
+export async function downloadFile(
 	arg: { url: string; saveToFile: boolean; destDir: string; returnFileContent: boolean },
-) => {
+) {
+	logger.debugFn(arguments);
+
 	const { url, destDir, saveToFile, returnFileContent } = arg;
 	const req = await fetch(url);
+	logger.debugVar('req', req);
 
 	if (!req.ok) {
 		await req.body?.cancel();
@@ -18,32 +22,47 @@ export const downloadFile = async (
 	}
 
 	const contentDisposition = req.headers.get('content-disposition');
+	logger.debugVar('contentDisposition', contentDisposition);
 
 	let filename = 'unknown';
+	logger.debugVar('filename', filename);
+
 	if (contentDisposition) {
 		const match = /filename[^;=\n]*=((['"]).*?\2|[^;\n]*)/.exec(contentDisposition);
 
 		if (match && match[1]) {
 			filename = match[1].replace(/['"]/g, '');
+			logger.debugVar('filename', filename);
 		}
 	} else {
 		const candidate = new URL(url).pathname.split('/').slice(-1)[0];
+		logger.debugVar('candidate', candidate);
 
 		if (candidate) {
 			filename = candidate;
+			logger.debugVar('filename', filename);
 		}
 	}
 
 	const contentLength = Number(req.headers.get('content-length'));
+	logger.debugVar('contentLength', contentLength);
+
 	const fileContent = new Uint8Array(contentLength);
+	logger.debugVar('fileContent', fileContent);
+
 	let downloadedBytes = 0;
+	logger.debugVar('downloadedBytes', downloadedBytes);
+
 	const classProgressBar = await import('https://deno.land/x/progress@v1.3.9/mod.ts');
+	logger.debugVar('classProgressBar', classProgressBar);
+
 	const progressBar = new classProgressBar.default({
 		total: 100,
 		interval: 15,
 		width: 10,
 		display: 'Downloading: :percent :bar t: :time eta: :eta',
 	});
+	logger.debugVar('progressBar', progressBar);
 
 	progressBar.render(0);
 
@@ -55,8 +74,10 @@ export const downloadFile = async (
 		fileContent.set(chunk, downloadedBytes);
 
 		downloadedBytes += chunk.length;
+		logger.debugVar('downloadedBytes', downloadedBytes);
 
 		const progress = Math.floor((downloadedBytes / contentLength) * 100);
+		logger.debugVar('progress', progress);
 
 		progressBar.render(progress);
 	}
@@ -70,26 +91,34 @@ export const downloadFile = async (
 	} = {
 		basename: filename,
 	};
+	logger.debugVar('details', details);
 
 	if (saveToFile) {
 		if (!await pathExist(destDir)) {
+			logger.debug('Make directory', destDir);
 			await Deno.mkdir(destDir, { recursive: true });
 		}
 
 		const filepath = `${destDir}/${filename}`;
+		logger.debugVar('filepath', filepath);
 
 		details.filename = filepath;
+		logger.debugVar('details.filename', details.filename);
 
 		if (await pathExist(filepath)) {
+			logger.debug('Remove', filepath);
 			await Deno.remove(filepath);
 		}
 
+		logger.debug('Write file', filepath, fileContent);
 		await Deno.writeFile(filepath, fileContent);
 	}
 
 	if (returnFileContent) {
 		details.fileContent = fileContent;
+		logger.debugVar('details.fileContent', details.fileContent);
 	}
 
+	logger.debugVar('details', details);
 	return details;
-};
+}
